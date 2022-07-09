34f555aa476dab88d54d8d8d648e243986486b55
Yander <103792433+Theyanderbot@users.noreply.github.com>
2022-07-09 00:39:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DED - Bot\nWhatsApp Bot DED', {
+say('THE DED -BOT\ DED BOT', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
