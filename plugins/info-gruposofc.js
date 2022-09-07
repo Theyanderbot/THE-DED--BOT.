3eac40c59a50361aa4083d6957ed08762f0787d1
Yander <103792433+Theyanderbot@users.noreply.github.com>
2022-09-06 18:15:03 -0600

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The DED - Bot*
 
 *➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/E6wWhcceZkR01sPntCsJ0V
+*1.-* https://chat.whatsapp.com/Ld9W7nyITsiHMeEo39DK7b
 
 `.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
 handler.command = /^linkgc|grupos$/i
